d2d3b9fb6bcf8a105b356be49e117078f758ae46
Juliana Connors <juliana.g.connors@gmail.com>
2020-08-21 23:11:31 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -29,7 +29,7 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age) {
-    case age >= 13 && age <= 19:
+    case ">= 13 && <= 19":
       return "Your are a teenager";
     default:
       return "You have an age";
